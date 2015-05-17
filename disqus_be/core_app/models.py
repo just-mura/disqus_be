7f966b786ee22b655d9f8bd09ac89e44443f1c3f
just-mura <muratbek.agabyl@ce.sdu.edu.kz>
2015-05-17 12:32:40 +0600

    my commit

--- disqus_be/core_app/models.py
+++ disqus_be/core_app/models.py
@@ -3,3 +3,9 @@ from django.db import models
 from django.utils import timezone
 from core_app import utils
 from hashlib import md5
+
+class Comment(models.Model):
+	author = models.CharField(max_length=30)
+	text = models.CharField(max_length=30)
+	pub_date = models.DateField(auto_now=True)
+	link = models.CharField(max_length=30)
--- disqus_be/urls.py
+++ disqus_be/urls.py
@@ -3,7 +3,11 @@
 Include your API resources and views into urlpatterns
 """
 from django.conf.urls import patterns, include, url
+from core_app.api import CommentResource
+
+comment = CommentResource()
 
 urlpatterns = patterns(
     '',
+    url(r'^api/v1/',  include(comment.urls)),
 )
